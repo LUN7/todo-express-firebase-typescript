74ffcdc8c31334d3dc14a6c1c57b96e8da92c438
LUN7 <kwl188836@gmail.com>
2021-08-11 10:26:25 +0800

    test: add test for retrieve request validator

--- src/controllers/test.ts
+++ src/controllers/test.ts
@@ -137,62 +137,83 @@ describe('validate update request', () => {
       expect(mockNextFunction.mock.calls.length).toBe(1)
       expect(mockNextFunction.mock.calls[0][0]).toBeDefined()
     })
+  })
+  it('should invoke next function with error if userId is provided', async () => {
+    const request = {
+      params: {
+        id: "abc"
+      },
+      body: {
+        "userId": "lun1e89234798"
+      }
+    } as unknown as express.Request
+    const mockNextFunction = jest.fn();
+    const mockResponse = {} as unknown as express.Response
+    await requestValidator.validateUpdate(request, mockResponse, mockNextFunction)
+    expect(mockNextFunction.mock.calls.length).toBe(1)
+    expect(mockNextFunction.mock.calls[0][0]).toBeDefined()
+  })
 
-    it('should invoke next function with error if userId is provided', async () => {
-      const request = {
-        params: {
-          id: "abc"
-        },
-        body: {
-          "userId": "lun1e89234798"
-        }
-      } as unknown as express.Request
-      const mockNextFunction = jest.fn();
-      const mockResponse = {} as unknown as express.Response
-      await requestValidator.validateUpdate(request, mockResponse, mockNextFunction)
-      expect(mockNextFunction.mock.calls.length).toBe(1)
-      expect(mockNextFunction.mock.calls[0][0]).toBeDefined()
-    })
+  it('should invoke next function with error if name is not string', async () => {
+    const request = {
+      params: {
+        id: "abc"
+      },
+      body: {
+        "userId": "lun1e89234798"
+      }
+    } as unknown as express.Request
+    const mockNextFunction = jest.fn();
+    const mockResponse = {} as unknown as express.Response
+    await requestValidator.validateUpdate(request, mockResponse, mockNextFunction)
+    expect(mockNextFunction.mock.calls.length).toBe(1)
+    expect(mockNextFunction.mock.calls[0][0]).toBeDefined()
+  })
 
-    it('should invoke next function with error if name is not string', async () => {
-      const request = {
-        params: {
-          id: "abc"
-        },
-        body: {
-          "userId": "lun1e89234798"
-        }
-      } as unknown as express.Request
-      const mockNextFunction = jest.fn();
-      const mockResponse = {} as unknown as express.Response
-      await requestValidator.validateUpdate(request, mockResponse, mockNextFunction)
-      expect(mockNextFunction.mock.calls.length).toBe(1)
-      expect(mockNextFunction.mock.calls[0][0]).toBeDefined()
-    })
-
-    it('should invoke next function with error if deadline is not timestamp', async () => {
-      const request = {
-        params: {
-          id: "abc"
-        },
-        body: {
-          "userId": "lun1e89234798"
-        }
-      } as unknown as express.Request
-      const mockNextFunction = jest.fn();
-      const mockResponse = {} as unknown as express.Response
-      await requestValidator.validateUpdate(request, mockResponse, mockNextFunction)
-      expect(mockNextFunction.mock.calls.length).toBe(1)
-      expect(mockNextFunction.mock.calls[0][0]).toBeDefined()
-    })
+  it('should invoke next function with error if deadline is not timestamp', async () => {
+    const request = {
+      params: {
+        id: "abc"
+      },
+      body: {
+        "userId": "lun1e89234798"
+      }
+    } as unknown as express.Request
+    const mockNextFunction = jest.fn();
+    const mockResponse = {} as unknown as express.Response
+    await requestValidator.validateUpdate(request, mockResponse, mockNextFunction)
+    expect(mockNextFunction.mock.calls.length).toBe(1)
+    expect(mockNextFunction.mock.calls[0][0]).toBeDefined()
   })
 })
 
-// describe('validate retrieve request', () => {
-//   it('should not throw error if request body is correct', () => {
-//     requestValidator.validateCreate()
-//   })
-// })
+describe('validate retrieve request', () => {
+  it('should invoke next function if id is provided', () => {
+    const request = {
+      params: {
+        id: "asdsa"
+      }
+    } as unknown as express.Request
+    const mockResponse = {} as unknown as express.Response
+    const mockNextFunction = jest.fn();
+    await requestValidator.validateRetireve(request, mockResponse, mockNextFunction)
+    expect(mockNextFunction.mock.calls.length).toBe(1)
+    expect(mockNextFunction.mock.calls[0][0]).toBeNull()
+  })
+  it('should invoke next function with error if id is not provided', () => {
+    const request = {
+      params: {
+        id: "asdsa"
+      }
+    } as unknown as express.Request
+    const mockResponse = {} as unknown as express.Response
+    const mockNextFunction = jest.fn();
+    requestValidator.validateRetireve(request, mockResponse, mockNextFunction)
+    expect(mockNextFunction.mock.calls.length).toBe(1)
+    expect(mockNextFunction.mock.calls[0][0]).toBeDefined()
+  })
+}
+)
 // describe('validate list request', () => {
 //   it('should not throw error if request body is correct', () => {
 //     requestValidator.validateCreate()
